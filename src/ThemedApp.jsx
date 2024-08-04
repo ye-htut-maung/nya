cb1a563adbe3efe0c220fc163f7678694402eb7a
ye-htut-maung <yehtutmaung21@gmail.com>
2024-08-04 17:34:58 -0400

    add menu

--- src/ThemedApp.jsx
+++ src/ThemedApp.jsx
@@ -1,22 +1,73 @@
-import { useState, createContext, useContext } from "react";
-import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { useState, createContext, useContext, useMemo } from "react";
+
+import {
+  CssBaseline,
+  ThemeProvider,
+  createTheme,
+  Snackbar,
+} from "@mui/material";
+
 import App from "./App";
-const theme = createTheme({
-  palette: {
-    mode: "dark",
-  },
-});
+import AppDrawer from "./components/AppDrawer";
+
+import { deepPurple, grey } from "@mui/material/colors";
+
 const AppContext = createContext();
+
 export function useApp() {
   return useContext(AppContext);
 }
 
 export default function ThemedApp() {
+  const [showDrawer, setShowDrawer] = useState(false);
   const [showForm, setShowForm] = useState(false);
+  const [globalMsg, setGlobalMsg] = useState(null);
+  const [auth, setAuth] = useState(null);
+  const [mode, setMode] = useState("dark");
+
+  const theme = useMemo(() => {
+    return createTheme({
+      palette: {
+        mode,
+        primary: deepPurple,
+        banner: mode === "dark" ? grey[800] : grey[200],
+        text: {
+          fade: grey[500],
+        },
+      },
+    });
+  }, [mode]);
+
   return (
     <ThemeProvider theme={theme}>
-      <AppContext.Provider value={{ showForm, setShowForm }}>
+      <AppContext.Provider
+        value={{
+          showDrawer,
+          setShowDrawer,
+          showForm,
+          setShowForm,
+          globalMsg,
+          setGlobalMsg,
+          auth,
+          setAuth,
+          mode,
+          setMode,
+        }}
+      >
         <App />
+        <AppDrawer />
+
+        <Snackbar
+          anchorOrigin={{
+            horizontal: "center",
+            vertical: "bottom",
+          }}
+          open={Boolean(globalMsg)}
+          autoHideDuration={6000}
+          onClose={() => setGlobalMsg(null)}
+          message={globalMsg}
+        />
+
         <CssBaseline />
       </AppContext.Provider>
     </ThemeProvider>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,51 @@
-import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material";
 import { useApp } from "../ThemedApp";
+
+import { Box, AppBar, Toolbar, Typography, IconButton } from "@mui/material";
 
 import {
   Menu as MenuIcon,
   Add as AddIcon,
   LightMode as LightModeIcon,
+  DarkMode as DarkModeIcon,
 } from "@mui/icons-material";
 
 export default function Header() {
-  const { showForm, setShowForm } = useApp();
+  const { setShowDrawer, showForm, setShowForm, mode, setMode } = useApp();
 
   return (
     <AppBar position="static">
       <Toolbar>
-        <IconButton color="inherit" edge="start">
+        <IconButton
+          color="inherit"
+          edge="start"
+          onClick={() => setShowDrawer(true)}
+        >
           <MenuIcon />
         </IconButton>
-        <Typography sx={{ flexGrow: 1, ml: 2 }}>Nya</Typography>
+        <Typography sx={{ flexGrow: 1, ml: 2 }}>Yaycha</Typography>
 
         <Box>
           <IconButton color="inherit" onClick={() => setShowForm(!showForm)}>
             <AddIcon />
           </IconButton>
-          <IconButton color="inherit" edge="end">
-            <LightModeIcon />
-          </IconButton>
+
+          {mode === "dark" ? (
+            <IconButton
+              color="inherit"
+              edge="end"
+              onClick={() => setMode("light")}
+            >
+              <LightModeIcon />
+            </IconButton>
+          ) : (
+            <IconButton
+              color="inherit"
+              edge="end"
+              onClick={() => setMode("dark")}
+            >
+              <DarkModeIcon />
+            </IconButton>
+          )}
         </Box>
       </Toolbar>
     </AppBar>
